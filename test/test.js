ff0034d9c7ef3c3bb7e2616a156d9d200c32061d
James Cramer <jcramer@deskviz.com>
2018-10-06 13:28:49 -0400

    some initial tests

--- test/test.js
+++ test/test.js
@@ -1,12 +1,43 @@
 const assert = require('assert');
 const bitcoinfiles = require('../index.js');
-const downloadFile = bitcoinfiles.bfp.downloadFile;
+const bfp = bitcoinfiles.bfp;
 describe('bitcoinfiles', function(){
+    describe('Cost estimating for uploads', function(){
+        it('estimates fee for multi-chunk outside of metadata chunk', function(){
+            let config = {
+                msgType: 1,
+                chunkCount: 2,
+                fileName: 'token-document',
+                fileExt: '.json',
+                fileSize: 378,
+                fileSha256Hex: '9900c8f6b6ba545b9b7cf0953765dca2d9e7fb378e664b3791401b8c2f4d8420',
+                prevFileSha256Hex: null,
+                fileUri: null,
+                chunkData: null
+            }
+
+            // estimate fee for file
+            let cost = bfp.calculateFileUploadCost(378, config);
+            assert.equal(cost, 1615)
+        });
+        it('estimates fee for single chunk in metadata', function(){
+
+        });
+        it('estimates fee for single chunk outside of metadata', function(){
+
+        });
+        it('estimates fee for multi-chunk including metadata chunk', function(){
+
+        });
+    });
     describe('download file', function(){
-        it('downloads a valid file', async function(){
+        it('downloads a file and hash valid file sha256', async function(){
             this.timeout(15000);
-            let buffer = await downloadFile('bitcoinfile:7e4600323c934926369c136562f5483e3df79baf087c8dd2b0ed1aea69d5ee49');
+            // download mario.png with size of 1723
+            let buffer = await bfp.downloadFile('bitcoinfile:7e4600323c934926369c136562f5483e3df79baf087c8dd2b0ed1aea69d5ee49');
             assert.equal(buffer.length, 1723)
+
+            //TODO: assert file hash equal
         });
     });
 });
