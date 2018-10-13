06a97e9f0c51bd22d3b3fbccc09aa01f728ce061
James Cramer <jcramer@deskviz.com>
2018-10-13 08:38:49 -0400

    add getFileMetadata method using bitdb/bitquery

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const BITBOXCli = require('bitbox-cli/lib/bitbox-cli').default
 
 let bfp = require('./lib/bfp');
 let utils = require('./lib/utils');
-let network = require('./lib/network')
+let network = require('./lib/network');
+let bitdb = require('./lib/bitdb');
 
 module.exports = {
     bfp: bfp,
     utils: utils, 
     network: network,
+    bitdb: bitdb,
     bitbox: BITBOX
 }
